47c33efb7ddcfe28fa9b1430d1a00e406ad6c31c
blaxsior <heejun2798@gmail.com>
2022-11-05 10:54:05 +0900

    의존성 가지는 라이브러리들의 버전을 최신으로 업데이트하고, mysql 및 exposed 라이브러리를 위한 의존성 추가

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,6 +2,9 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
+val exposedVersion: String by project
+val mysqlVersion: String by project
+
 plugins {
     application
     kotlin("jvm") version "1.7.20"
@@ -23,6 +26,7 @@ repositories {
 }
 
 dependencies {
+    // 서버 관련
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-freemarker-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
@@ -30,7 +34,14 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    // 데이터베이스 관련
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
+    implementation("mysql:mysql-connector-java:$mysqlVersion")
+    // 테스트 관련
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- server/src/main/kotlin/com/example/Application.kt
+++ server/src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,13 @@
 package com.example
 
+import com.example.entity.dbTest
 import io.ktor.server.application.*
 import com.example.plugins.*
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>){
+    dbTest("id","passsword", "test")
+    //io.ktor.server.netty.EngineMain.main(args)
+}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
