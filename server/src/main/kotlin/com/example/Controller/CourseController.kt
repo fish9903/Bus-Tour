126755c1792b5e937034a318b393a5738e1103cc
blaxsior <heejun2798@gmail.com>
2022-11-15 15:16:08 +0900

    add thumbnail column to Course Entity & fix CourseController can eager loading

--- server/src/main/kotlin/com/example/Controller/CourseController.kt
+++ server/src/main/kotlin/com/example/Controller/CourseController.kt
@@ -3,6 +3,7 @@ package com.example.Controller
 import com.example.entity.CourseEntity
 import com.example.entity.CourseWithPrograms
 import com.example.entity.CourseWithThumbnail
+import org.jetbrains.exposed.dao.with
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class CourseController {
@@ -12,6 +13,9 @@ class CourseController {
     }
     // program과 함께 반환
     fun getAllwithProgram(): Iterable<CourseWithPrograms> = transaction {
-        CourseEntity.all().map(CourseEntity::getCourseWithPrograms)
+        CourseEntity.all().with(
+            CourseEntity::programs,
+            CourseEntity::priceinfos
+        ).map(CourseEntity::getCourseWithPrograms)
     }
 }
--- server/src/main/kotlin/com/example/entity/CourseEntity.kt
+++ server/src/main/kotlin/com/example/entity/CourseEntity.kt
@@ -9,12 +9,15 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 /**
  * Course 엔티티의 테이블 정의
  * @property name 코스의 이름
+ * @property thumbnail 이미지의 주소
  * @property short_desc 코스에 대한 짧은 묘사( 아름다운 속초 !)
  * @property description 코스에 대한 문서 내용
  * ( eager loading -> false )이므로, 코스에 대한 세부 내용을 가져올때 명시적으로 eager load 수행.
  */
 object Courses: IntIdTable() {
     val name = varchar("name", 32)
+    val thumbnail = varchar("thumbnail", 100)
+
     val short_desc = varchar("short_desc", 100)
     // 반드시 가져오지는 않는다.
     val description = text("description", eagerLoading = false)
@@ -24,6 +27,7 @@ object Courses: IntIdTable() {
 class CourseEntity(id:EntityID<Int>): IntEntity(id) {
     companion object: IntEntityClass<CourseEntity>(Courses)
     var name by Courses.name
+    var thumbnail by Courses.thumbnail
     var short_desc by Courses.short_desc
     var description by Courses.description
 
@@ -34,6 +38,7 @@ class CourseEntity(id:EntityID<Int>): IntEntity(id) {
     fun getCourseWithPrograms()= CourseWithPrograms(
         this.id.value,
         this.name,
+        this.thumbnail,
         this.short_desc,
         this.description,
         this.programs.map {
@@ -47,6 +52,7 @@ class CourseEntity(id:EntityID<Int>): IntEntity(id) {
     fun getCourseWithThumbnail() = CourseWithThumbnail(
         this.id.value,
         this.name,
+        this.thumbnail,
         this.short_desc
     )
 }
@@ -55,6 +61,7 @@ class CourseEntity(id:EntityID<Int>): IntEntity(id) {
 data class CourseWithThumbnail(
     val id: Int,
     val name: String,
+    val thumbnail: String,
     val short_desc: String
 )
 
@@ -62,6 +69,7 @@ data class CourseWithThumbnail(
 data class CourseWithPrograms(
     val id: Int,
     val name: String,
+    val thumbnail: String,
     val short_desc: String,
     val description: String,
     val programs: List<Program>,
